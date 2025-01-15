fc60e3be1f73569c6e49c3fe91ed36b560e73a32
Majito Brinkitos <mcvef2013@gmail.com>
2025-01-14 21:35:48 -0800

    Exercise1.js

--- exercise1.js
+++ exercise1.js
@@ -10,22 +10,31 @@ class Car {
     this.year = year;
   }
 
+  getYear() { 
+    return this.year; 
+  }
+
   getCarInfo() {
-    console.log(`The car's profile is: ${this.make}, ${this.model}, ${this.year}`);
+    return(`The car's profile is: ${this.make}, ${this.model}, ${this.year}`);
   }
 
-  setYear(newYear) :function() {
-    return new Date().getFullYear {
-      if ()
+  setYear(newYear) {
+
+    if(newYear >= this.year) {
+      this.year = newYear;
+      console.log(`The year has been updated to ${newYear}.`);
+    } else {
+      console.log(`The new year couldn't be updated (${this.year}).`);
     }
-  }
 }
-
+}
 
 // TEST CASE / DRIVER CODE
 const car1 = new Car("Toyota", "Corolla", 2010);
 console.log(car1.getCarInfo()); // Expected output: "Make: Toyota, Model: Corolla, Year: 2010"
+
 car1.setYear(2015); // Should update the year
 console.log(car1.getCarInfo()); // Expected output: "Make: Toyota, Model: Corolla, Year: 2015"
+
 car1.setYear(2005); // Should not update the year
 console.log(car1.getCarInfo()); // Expected output: "Make: Toyota, Model: Corolla, Year: 2015"
